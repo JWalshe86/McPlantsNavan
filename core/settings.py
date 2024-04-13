66d6aa1187de5a51e0f567e1fb4e31e3e0bb15e7
JWalshe86 <walshejohnnyw7>
2024-04-13 15:21:14 +0100

    adds plant images to db

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
-
+    'plants',
 ]
 
 MIDDLEWARE = [
---
+++ plants/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Plant, Category
+
+
+admin.site.register(Plant)
+admin.site.register(Category)
+
