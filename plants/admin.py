34f282aa3a90bb6f8ceedc147b91155c9ad77353
JWalshe86 <walshejohnnyw7>
2024-04-14 13:05:22 +0100

    Customised admin

--- plants/admin.py
+++ plants/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import Plant, Category
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
 
-admin.site.register(Plant)
-admin.site.register(Category)
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+
+admin.site.register(Plant, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
 
