0a0e382539de28e08949d70f31a887000cb60f00
JWalshe86 <walshejohnnyw7>
2024-04-14 18:13:33 +0100

    Adds plants templates and views

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('plants/', include('plants.urls')),
   ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- plants/views.py
+++ plants/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils.http import content_disposition_header
+from .models import Plant
 
-# Create your views here.
+def all_plants(request):
+    """A view to show all products, including sorting and search queries """
+
+    products = Plant.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'plants/plants.html', context)
