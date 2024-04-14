3af3438cbf65352f8c401a316fe8d0ec91403055
JWalshe86 <walshejohnnyw7>
2024-04-14 19:52:59 +0100

    Adds css plant images same size

--- plants/views.py
+++ plants/views.py
@@ -5,9 +5,9 @@ from .models import Plant
 def all_plants(request):
     """A view to show all products, including sorting and search queries """
 
-    products = Plant.objects.all()
+    plants = Plant.objects.all()
 
     context = {
-        'products': products,
+        'plants': plants,
     }
     return render(request, 'plants/plants.html', context)
