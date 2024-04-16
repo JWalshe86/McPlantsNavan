ebcecbc30f4625e0a5aa9225500c0d9caa0e16e1
JWalshe86 <walshejohnnyw7>
2024-04-16 20:34:56 +0100

    Removes category col from plants.json

--- core/settings.py
+++ core/settings.py
@@ -145,8 +145,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = '"$(wslpath "$(wslvar USERPROFILE)")"/sites/McplantsNavan/static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
