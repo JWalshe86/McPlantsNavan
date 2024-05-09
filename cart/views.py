99d661d0e678dc16cde55bae8e3436f639dfa7ab
JWalshe86 <walshejohnnyw7@gmail.com>
2024-05-09 22:10:07 +0100

    Adds adjust_cart view

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, reverse
 
 
 def view_cart(request):
@@ -10,20 +10,20 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """Add quantity of specified plant to cart"""
     quantity = int(request.POST.get("quantity"))
-    redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.POST.get("redirect_url")
     size = None
-    if 'plant_size' in request.POST:
-        size = request.POST['plant_size']
+    if "plant_size" in request.POST:
+        size = request.POST["plant_size"]
     cart = request.session.get("cart", {})
 
     if size:
         if item_id in list(cart.keys()):
-            if size in cart[item_id]['items_by_size'].keys():
-                cart[item_id]['items_by_size'][size] += quantity
+            if size in cart[item_id]["items_by_size"].keys():
+                cart[item_id]["items_by_size"][size] += quantity
             else:
-                cart[item_id]['items_by_size'][size] = quantity
+                cart[item_id]["items_by_size"][size] = quantity
         else:
-            cart[item_id] = {'items_by_size': {size: quantity}}
+            cart[item_id] = {"items_by_size": {size: quantity}}
     else:
 
         if item_id in list(cart.keys()):
@@ -33,3 +33,28 @@ def add_to_cart(request, item_id):
 
     request.session["cart"] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust teh quantity of the specified plant to the specified amount"""
+
+    quantity = int(request.POST.get("quantity"))
+    size = None
+    if "plant_size" in request.POST:
+        size = request.POST["plant_size"]
+    cart = request.session.get("cart", {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]["items_by_size"][size] = quantity
+        else:
+            del cart[item_id]["items_by_size"][size]
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop[item_id]
+
+
+    request.session["cart"] = cart
+    return redirect(reverse('view_cart'))
