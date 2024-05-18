cefda5fcfef7db0859fd69d19ceb300bbda55681
John Walshe <walshejohnnyw7@gmail.com>
2024-05-18 19:51:23 +0100

    Adds checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+form django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart at the moment")
+        return redirect(reverse('plants'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+            'order_form': order_form,
+            }
+
+    return render(request, template, context)
