9d26049b1740e62e27167e82d6741524ee56ee75
John Walshe <walshejohnnyw7@gmail.com>
2024-05-20 19:32:58 +0100

    Add stripe key functionality to view

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,11 @@ from cart.contexts import cart_contents
 import stripe
 
 def checkout(request):
+    
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
@@ -16,14 +21,26 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']    
     stripe_total = round(total * 100 )
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
-    
+    print(intent)   
+
+ 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+             Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
             'order_form': order_form,
-            'stripe_public_key': 'pk_test_51Of16vD1UHgIZx0CYc1lVjAdJPVYTbJcWVKMbi7Erxri5i9iyEDlTfUUGGlydDT08A3JwYUHArd11vcc7VjwGVdD00gUnB4OZm',
-            'client_secret': 'test client secret',
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
             }
 
     return render(request, template, context)
