29c998f37d0406cc57db1891e9cec98766f96659
John Walshe <walshejohnnyw7@gmail.com>
2024-05-20 16:01:58 +0100

    Add stripe functionality to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe
 
 def checkout(request):
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('plants'))
+    
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']    
+    stripe_total = round(total * 100 )
 
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
