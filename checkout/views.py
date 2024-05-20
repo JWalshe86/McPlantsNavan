a1cc4705be91139cb328940e2c643a483f10f6b3
John Walshe <walshejohnnyw7@gmail.com>
2024-05-20 23:44:15 +0100

    Add error messages & checkout success view

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from plants.models import Plant
+from .models import Order, OrderLineItem
 from cart.contexts import cart_contents
 
 import stripe
@@ -12,35 +14,119 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your cart at the moment")
-        return redirect(reverse('plants'))
-    
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']    
-    stripe_total = round(total * 100 )
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = { 
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+				'phone_number': request.POST['phone_number'],
+				'country': request.POST['country'],
+				'postcode': request.POST['postcode'],
+				'town_or_city': request.POST['town_or_city'],
+				'street_address1': request.POST['street_address1'],
+				'street_address2': request.POST['street_address2'],
+				'county': request.POST['county'],
 
-    print(intent)   
+                }
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "There's nothing in your cart at the moment")
+            return redirect(reverse('plants'))
+        
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']    
+        stripe_total = round(total * 100 )
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
- 
-    order_form = OrderForm()
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order_form.save()
+            for item_id, item_data in cart.items(): 
+                try:
+                    plant = Plant.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            plant=plant,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                plant=plant,
+                                quantity=quantity,
+                                plant_size=size,
+                            )
+                            order_line_item.save()
+                except Plant.DoesNotExist:
+                    messages.error(request, (
+                        "One of the plants in your cart wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-             Did you forget to set it in your environment?')
+                request.session['save_info'] = 'save-info' in request.POST
+                return redirect(reverse('checkout_success', args=[order.order_number]))
+            else:
+                messages.error(request, "One of the plants in your cart wasn't found in our database"
+                        "Please call us for assistance!")
+        else:
+            cart = request.session.get('cart', {})
+            if not cart:
+                meesages.error(request, "There's nothing in your cart at the moment")
+                return redirect(reverse('plants'))
 
-    template = 'checkout/checkout.html'
+
+                current_cart = cart_contents(request)
+                total = current_cart['grand_total']
+                stripe_total = round(total * 100)
+                stripe.api_key = stripe_secret_key
+                intent = stripe.PaymentIntent.create(
+                    amount=stripe_total,
+                    currency=settings.STRIPE_CURRENCY,
+                )
+
+                order_form = OrderForm()
+
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                 Did you forget to set it in your environment?')
+
+        template = 'checkout/checkout.html'
+        context = {
+                'order_form': order_form,
+                'stripe_public_key': stripe_public_key,
+                'client_secret': intent.client_secret,
+                }
+
+        return render(request, template, context)
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+            Your order number is {order_number}. A confirmation \
+            email will be sent to {order.email}.')
+
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/checkout_success.html'
     context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': intent.client_secret,
+        'order': order,
             }
 
     return render(request, template, context)
