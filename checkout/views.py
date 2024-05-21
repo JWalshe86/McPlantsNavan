751c8807f8e0660833dd7e0a7e20f5c4c554525e
John Walshe <walshejohnnyw7@gmail.com>
2024-05-21 20:59:45 +0100

    Loading overlay not displaying

--- checkout/views.py
+++ checkout/views.py
@@ -14,19 +14,19 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == 'POST':
-        cart = request.session.get('cart', {})
+    if request.method == "POST":
+        cart = request.session.get("cart", {})
 
         form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
+            "full_name": request.POST["full_name"],
+            "email": request.POST["email"],
+            "phone_number": request.POST["phone_number"],
+            "country": request.POST["country"],
+            "postcode": request.POST["postcode"],
+            "town_or_city": request.POST["town_or_city"],
+            "street_address1": request.POST["street_address1"],
+            "street_address2": request.POST["street_address2"],
+            "county": request.POST["county"],
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
@@ -42,7 +42,7 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in item_data["items_by_size"].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 plant=plant,
@@ -51,26 +51,32 @@ def checkout(request):
                             )
                             order_line_item.save()
                 except Plant.DoesNotExist:
-                    messages.error(request, (
-                        "One of the plants in your cart wasn't found in our database. "
-                        "Please call us for assistance!")
+                    messages.error(
+                        request,
+                        (
+                            "One of the plants in your cart wasn't found in our database. "
+                            "Please call us for assistance!"
+                        ),
                     )
                     order.delete()
-                    return redirect(reverse('view_cart'))
+                    return redirect(reverse("view_cart"))
 
-            request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            request.session["save_info"] = "save-info" in request.POST
+            return redirect(reverse("checkout_success", args=[order.order_number]))
         else:
-            messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
+            messages.error(
+                request,
+                "There was an error with your form. \
+                Please double check your information.",
+            )
     else:
-        cart = request.session.get('cart', {})
+        cart = request.session.get("cart", {})
         if not cart:
             messages.error(request, "There's nothing in your cart at the moment")
-            return redirect(reverse('plants'))
+            return redirect(reverse("plants"))
 
         current_cart = cart_contents(request)
-        total = current_cart['grand_total']
+        total = current_cart["grand_total"]
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
@@ -81,14 +87,17 @@ def checkout(request):
         order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        messages.warning(
+            request,
+            "Stripe public key is missing. \
+            Did you forget to set it in your environment?",
+        )
 
-    template = 'checkout/checkout.html'
+    template = "checkout/checkout.html"
     context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        "order_form": order_form,
+        "stripe_public_key": stripe_public_key,
+        "client_secret": intent.client_secret,
     }
 
     return render(request, template, context)
@@ -98,18 +107,21 @@ def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
-    save_info = request.session.get('save_info')
+    save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order successfully processed! \
+    messages.success(
+        request,
+        f"Order successfully processed! \
         Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        email will be sent to {order.email}.",
+    )
 
-    if 'cart' in request.session:
-        del request.session['cart']
+    if "cart" in request.session:
+        del request.session["cart"]
 
-    template = 'checkout/checkout_success.html'
+    template = "checkout/checkout_success.html"
     context = {
-        'order': order,
+        "order": order,
     }
 
     return render(request, template, context)
