32af04514c58415b9c3125759b080affcb72aec6
John Walshe <walshejohnnyw7@gmail.com>
2024-05-21 15:02:03 +0100

    Corrections to checkout view so GET response runs

--- checkout/views.py
+++ checkout/views.py
@@ -3,36 +3,35 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from plants.models import Plant
 from .models import Order, OrderLineItem
+from plants.models import Plant
 from cart.contexts import cart_contents
 
 import stripe
 
+
 def checkout(request):
-    
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
         cart = request.session.get('cart', {})
 
-        form_data = { 
-                'full_name': request.POST['full_name'],
-                'email': request.POST['email'],
-				'phone_number': request.POST['phone_number'],
-				'country': request.POST['country'],
-				'postcode': request.POST['postcode'],
-				'town_or_city': request.POST['town_or_city'],
-				'street_address1': request.POST['street_address1'],
-				'street_address2': request.POST['street_address2'],
-				'county': request.POST['county'],
-
-                }
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order_form.save()
-            for item_id, item_data in cart.items(): 
+            order = order_form.save()
+            for item_id, item_data in cart.items():
                 try:
                     plant = Plant.objects.get(id=item_id)
                     if isinstance(item_data, int):
@@ -59,42 +58,41 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_cart'))
 
-                request.session['save_info'] = 'save-info' in request.POST
-                return redirect(reverse('checkout_success', args=[order.order_number]))
-            else:
-                messages.error(request, "There was an error with your form." \
-                        "Please double check your information")
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            cart = request.session.get('cart', {})
-            if not cart:
-                messages.error(request, "There's nothing in your cart at the moment")
-                return redirect(reverse('plants'))
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "There's nothing in your cart at the moment")
+            return redirect(reverse('plants'))
 
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
-                current_cart = cart_contents(request)
-                total = current_cart['grand_total']
-                stripe_total = round(total * 100)
-                stripe.api_key = stripe_secret_key
-                intent = stripe.PaymentIntent.create(
-                    amount=stripe_total,
-                    currency=settings.STRIPE_CURRENCY,
-                )
+        order_form = OrderForm()
 
-                order_form = OrderForm()
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. \
-                 Did you forget to set it in your environment?')
+    return render(request, template, context)
 
-        template = 'checkout/checkout.html'
-        context = {
-                'order_form': order_form,
-                'stripe_public_key': stripe_public_key,
-                'client_secret': intent.client_secret,
-                }
-
-        return render(request, template, context)
 
 def checkout_success(request, order_number):
     """
@@ -103,8 +101,8 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Order successfully processed! \
-            Your order number is {order_number}. A confirmation \
-            email will be sent to {order.email}.')
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     if 'cart' in request.session:
         del request.session['cart']
@@ -112,6 +110,6 @@ def checkout_success(request, order_number):
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-            }
+    }
 
     return render(request, template, context)
