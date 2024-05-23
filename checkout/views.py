eed0b9feb612512c2fb3606dc21eadde754a407d
John Walshe <walshejohnnyw7@gmail.com>
2024-05-23 13:55:38 +0100

    Adds json data to cache_checkout_data

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,26 @@ from plants.models import Plant
 from cart.contexts import cart_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    pid = request.POST.get("client_secret").split("_secret")[0]
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripe.PaymentIntent.modify(
+        pid,
+        metadata={
+            "cart": json.dumps(request.session.get("cart", {})),
+            "save_info": request.POST.get("save_info"),
+            "username": request.user,
+        },
+    )
+    return HttpResponse(status=200)
+    expect Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+                processed right now. Please try again later.')
+        return HttpResponse(content=3, status=400)
 
 
 def checkout(request):
