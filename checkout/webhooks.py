38bfc367672cec9ad980b97e123c2a619510f5e5
John Walshe <walshejohnnyw7@gmail.com>
2024-05-23 21:01:04 +0100

    Return to using @csrf_exempt

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,14 +1,14 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 
 from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
 
-@csrf_protect
+@csrf_exempt
 @require_POST
 def webhook(request):
     """Listen for webhooks from Stripe"""
