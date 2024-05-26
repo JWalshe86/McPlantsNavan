dc84e36254837680464711648f5c26a766018167
John Walshe <walshejohnnyw7@gmail.com>
2024-05-26 23:47:31 +0100

    Found billing details in stripe_charge

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+import stripe
+
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -20,6 +22,19 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
+        pid = intent.id
+        cart = intent.metadata.cart
+        save_info = intent.metadata.save_info
+
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
+        print("stripecharge", stripe_charge)
+        # stripe charge shows billing details
+        billing_details = stripe_charge.billing_details  # updated
+        print("billingdeits", billing_details)
+        shipping_details = intent.shipping
+        grand_total = round(stripe_charge.amount / 100, 2)  # updated
+
         print(intent)
         return HttpResponse(content=f'Webhook received: {event["type"]}', status=200)
 
