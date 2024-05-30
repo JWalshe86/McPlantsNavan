9320bd982509e05321c1450f28eca74a26c5090c
John Walshe <walshejohnnyw7@gmail.com>
2024-05-30 16:30:48 +0100

    Add profile content to checkoutsucc html

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.profile, name="profile")]
+urlpatterns = [
+    path("", views.profile, name="profile"),
+    path("order_history/<order_number>", views.order_history, name="order_history"),
+]
