d8f35fac7e773e7ac12617dd719fc75fa62da991
John Walshe <walshejohnnyw7@gmail.com>
2024-06-05 23:22:40 +0100

    Creates 404 error page

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,3 +14,4 @@ urlpatterns = [
     path("checkout/", include("checkout.urls")),
     path("profiles/", include("profiles.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = "core.views.handler404"
