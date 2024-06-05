91154387d847dd572fc13dab611abe72f5320120
John Walshe <walshejohnnyw7@gmail.com>
2024-06-05 23:58:42 +0100

    Adds serve to settings.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import handler404
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -13,5 +14,6 @@ urlpatterns = [
     path("cart/", include("cart.urls")),
     path("checkout/", include("checkout.urls")),
     path("profiles/", include("profiles.urls")),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = "core.views.handler404"
