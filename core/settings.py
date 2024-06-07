c401d1f5365adeb081a720ad6c7297b31bbac9a3
John Walshe <walshejohnnyw7@gmail.com>
2024-06-07 18:07:44 +0100

    Changes to STATIC_ROOT

--- core/settings.py
+++ core/settings.py
@@ -169,8 +169,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-# STATIC_ROOT = '"$(wslpath "$(wslvar USERPROFILE)")"/sites/McplantsNavan/static'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
