3a2399cde46b261b17b39aa8ec1bcd75385e4a05
John Walshe <walshejohnnyw7@gmail.com>
2024-06-07 19:30:12 +0100

    adds static directory

--- core/settings.py
+++ core/settings.py
@@ -193,7 +193,7 @@ if "USE_AWS" in os.environ:
     MEDIAFILES_LOCATION = "media"
 
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/{MEDIAFILES_LOCATION}/"
 
 
 # Stripe Config
