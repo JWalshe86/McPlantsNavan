39b89802521e00aebeb55625d6bb70f0b8628abb
John Walshe <walshejohnnyw7@gmail.com>
2024-06-07 19:26:04 +0100

    Removes /static/

--- core/settings.py
+++ core/settings.py
@@ -185,9 +185,7 @@ if "USE_AWS" in os.environ:
     AWS_S3_REGION_NAME = "eu-north-1"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_CUSTOM_DOMAIN = (
-        f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-north-1.amazonaws.com/static"
-    )
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-north-1.amazonaws.com"
 
     STATICFILES_STORAGE = "custom_storages.StaticStorage"
     STATICFILES_LOCATION = "static"
