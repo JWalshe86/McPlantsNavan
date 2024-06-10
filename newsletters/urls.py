5392ea4dfe643ad67b3fdee85ee1ca4e749e64a8
John Walshe <walshejohnnyw7@gmail.com>
2024-06-10 22:36:21 +0100

    Create newsletter app urls

---
+++ newsletters/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+
+from .views import newsletter_signup, newsletter_unsubscribe
+
+urlpatterns = [
+    re_path(r'sign_up/$', newsletter_signup, name="newsletter_signup"),
+    re_path(r'unsubscribe/$', newsletter_unsubscribe, name='newsletter_unsubscribe'),
+        ]
