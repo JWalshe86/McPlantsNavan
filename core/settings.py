59a2970ec28d783c2a9d57a300ac295f7ccf8cde
John Walshe <walshejohnnyw7@gmail.com>
2024-06-11 22:48:36 +0100

    Changes media files loc to static/media

--- core/settings.py
+++ core/settings.py
@@ -190,7 +190,7 @@ if "USE_AWS" in os.environ:
     STATICFILES_STORAGE = "custom_storages.StaticStorage"
     STATICFILES_LOCATION = "static"
     DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
-    MEDIAFILES_LOCATION = "media"
+    MEDIAFILES_LOCATION = "static/media"
 
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
