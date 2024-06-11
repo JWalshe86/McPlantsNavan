c897668ff4ba7d660cbee3c47776c756621ba6e1
John Walshe <walshejohnnyw7@gmail.com>
2024-06-12 00:34:20 +0100

    Adds wh sh hardcoded

--- core/settings.py
+++ core/settings.py
@@ -200,8 +200,8 @@ if "USE_AWS" in os.environ:
 STRIPE_CURRENCY = "usd"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
-STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
-# STRIPE_WH_SECRET = "whsec_1f7HtG15MVhLGtXkkjke2fOz7AXGqhZ0"  # deployed
+# STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
+STRIPE_WH_SECRET = "whsec_1f7HtG15MVhLGtXkkjke2fOz7AXGqhZ0"  # deployed
 
 if "DEVELOPMENT" in os.environ:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
