70846c9d9791e499ca3ae3249fea7fceca93d49e
John Walshe <walshejohnnyw7@gmail.com>
2024-06-12 00:14:00 +0100

    Move review urls to bottom

--- plants/urls.py
+++ plants/urls.py
@@ -3,16 +3,16 @@ from . import views
 
 urlpatterns = [
     path("", views.all_plants, name="plants"),
-    path("all_reviews", views.all_reviews, name="reviews"),
     path("add/", views.add_plant, name="add_plant"),
-    path("add_review/", views.add_review, name="add_review"),
-    path("edit_review/<int:review_id>/", views.edit_review, name="edit_review"),
-    path("delete/<int:review_id>/", views.delete_review, name="delete_review"),
-    path("<int:review_id>/", views.review_detail, name="review_detail"),
     path("<int:plant_id>/", views.plant_detail, name="plant_detail"),
     path("edit/<int:plant_id>/", views.edit_plant, name="edit_plant"),
     path("delete/<int:plant_id>/", views.delete_plant, name="delete_plant"),
     path("all_events", views.all_events, name="all_events"),
     path("add_event", views.add_event, name="add_event"),
+    path("all_reviews", views.all_reviews, name="reviews"),
+    path("add_review/", views.add_review, name="add_review"),
+    path("<int:review_id>/", views.review_detail, name="review_detail"),
+    path("edit_review/<int:review_id>/", views.edit_review, name="edit_review"),
+    path("delete/<int:review_id>/", views.delete_review, name="delete_review"),
     path("stock_display", views.stock_display, name="stock_display"),
 ]
