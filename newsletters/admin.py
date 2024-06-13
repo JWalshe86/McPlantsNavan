c49fea584506e72730249618103e0c2684f72d12
John Walshe <walshejohnnyw7@gmail.com>
2024-06-13 22:51:55 +0100

    Adds newsletter model & admin

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 
-from .models import NewsletterUser
+from .models import NewsletterUser, Newsletter
+
 
 class NewsletterAdmin(admin.ModelAdmin):
-    list_display = ('email', 'date_added',)
+    list_display = (
+        "email",
+        "date_added",
+    )
+
 
 admin.site.register(NewsletterUser, NewsletterAdmin)
 
+admin.site.register(Newsletter)
