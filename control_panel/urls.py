8d2165f36070e7eade812e6d0b61db0e4ae98f87
John Walshe <walshejohnnyw7@gmail.com>
2024-06-14 21:21:48 +0100

    Adds newsletter list html

--- control_panel/urls.py
+++ control_panel/urls.py
@@ -1,7 +1,10 @@
 from django.urls import re_path
 
-from newsletters.views import control_newsletter
+from newsletters.views import control_newsletter, control_newsletter_list
 
 urlpatterns = [
     re_path(r"^newsletter/", control_newsletter, name="control_newsletter"),
+    re_path(
+        r"newsletter-list", control_newsletter_list, name="control_newsletter_list"
+    ),
 ]
