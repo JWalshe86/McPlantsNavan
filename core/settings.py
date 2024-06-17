4e8ffaf833c9fcd7620e42e0a598e25f4068fb34
John Walshe <walshejohnnyw7@gmail.com>
2024-06-17 21:14:37 +0100

    :test_tube' Hard code stripe wh secret

--- core/settings.py
+++ core/settings.py
@@ -200,12 +200,14 @@ if "USE_AWS" in os.environ:
 STRIPE_CURRENCY = "usd"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
-STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
+# STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
+STRIPE_WH_SECRET = "whsec_1f7HtG15MVhLGtXkkjke2fOz7AXGqhZ0"  # deployed
+
 
 if "DEVELOPMENT" in os.environ:
-    DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASS")
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
@@ -215,6 +217,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = "smtp.gmail.com"
-    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
-    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
