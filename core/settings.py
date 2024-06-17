59d4298b377c25a78e5831259e48e27dda0db4c3
John Walshe <walshejohnnyw7@gmail.com>
2024-06-17 21:25:56 +0100

    :bug: Change WB_SECRET TO WH_SECRET HEROKU

--- core/settings.py
+++ core/settings.py
@@ -200,8 +200,7 @@ if "USE_AWS" in os.environ:
 STRIPE_CURRENCY = "usd"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
-# STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
-STRIPE_WH_SECRET = "whsec_1f7HtG15MVhLGtXkkjke2fOz7AXGqhZ0"  # deployed
+STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
 
 if "DEVELOPMENT" in os.environ:
